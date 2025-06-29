d4b6193b186141aa3b3a8e69408ada7b51a46510
wardDward <edward.taligatos03@gmail.com>
2025-06-29 20:49:23 +0800

    refactor: get to post method

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { login, register } from "../controller/auth/auth.controller"
+import { login, refreshToken, register } from "../controller/auth/auth.controller"
 
 
 const authRouter = Router()
 
 authRouter.post('/register', register)
 authRouter.post('/login', login)
+authRouter.post('/refresh_token', refreshToken)
 
 export default authRouter
