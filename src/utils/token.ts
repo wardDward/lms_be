5c16264a22a6db0bf0f317e27df6e9acec548b15
wardDward <edward.taligatos03@gmail.com>
2025-06-29 20:49:53 +0800

    fix: _id to id

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,11 +2,11 @@ import jwt from 'jsonwebtoken'
 
 // Generate a short-lived access token (5s for demo; ~15min for real apps)
 export const generateAccessToken = (user : any) => {
-    const access_token = jwt.sign( {id: user._id}, process.env.ACCESS_TOKEN!, {expiresIn: '5s'})   
+    const access_token = jwt.sign( {id: user.id}, process.env.ACCESS_TOKEN!, {expiresIn: '5s'})   
     return access_token
 }
 
 export const generateRefreshToken = (user:any) => {
-    const refresh_token = jwt.sign({id: user._id}, process.env.REFRESK_TOKEN!, {expiresIn: '7d'})
+    const refresh_token = jwt.sign({id: user.id}, process.env.REFRESH_TOKEN!, {expiresIn: '7d'})
     return refresh_token
 }
