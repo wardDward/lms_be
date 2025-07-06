067a5c6b164dbd9ddb04b122eba218bf57599858
wardDward <edward.taligatos03@gmail.com>
2025-07-06 21:53:35 +0800

    fix: user token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,11 +2,11 @@ import jwt from 'jsonwebtoken'
 
 // Generate a short-lived access token (5s for demo; ~15min for real apps)
 export const generateAccessToken = (user : any) => {
-    const access_token = jwt.sign( {id: user.id}, process.env.ACCESS_TOKEN!, {expiresIn: '50s'})   
+    const access_token = jwt.sign( user, process.env.ACCESS_TOKEN!, {expiresIn: '15m'})   
     return access_token
 }
 
 export const generateRefreshToken = (user:any) => {
-    const refresh_token = jwt.sign({id: user.id}, process.env.REFRESH_TOKEN!, {expiresIn: '7d'})
+    const refresh_token = jwt.sign(user, process.env.REFRESH_TOKEN!, {expiresIn: '7d'})
     return refresh_token
 }
