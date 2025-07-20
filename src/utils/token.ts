5a9a47c8ca6b2d4387d442a1b088f4a2f7c2913e
wardDward <edward.taligatos03@gmail.com>
2025-07-21 00:39:03 +0800

    chore: pass user_id

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken'
 
 // Generate a short-lived access token (5s for demo; ~15min for real apps)
-export const generateAccessToken = (user : any) => {
-    const access_token = jwt.sign( user, process.env.ACCESS_TOKEN!, {expiresIn: '1d'})   
+export const generateAccessToken = (user_id: number) => {
+    const access_token = jwt.sign({ sub: user_id }, process.env.ACCESS_TOKEN!, { expiresIn: '1d' })
     return access_token
 }
 
-export const generateRefreshToken = (user:any) => {
-    const refresh_token = jwt.sign(user, process.env.REFRESH_TOKEN!, {expiresIn: '7d'})
+export const generateRefreshToken = (user_id: number) => {
+    const refresh_token = jwt.sign({ sub: user_id }, process.env.REFRESH_TOKEN!, { expiresIn: '7d' })
     return refresh_token
 }
