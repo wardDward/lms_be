d5def9c61dbbbb79a0637617ebfe765267795c5d
wardDward <edward.taligatos03@gmail.com>
2025-07-21 00:38:25 +0800

    WIP: course controller

--- src/controller/instructor/course.controller.ts
+++ src/controller/instructor/course.controller.ts
@@ -1,1 +1,36 @@
-// course countreller
+import { Request, Response } from "express";
+import { PrismaClient } from "../../../generated/prisma";
+import expressAsyncHandler from "express-async-handler";
+
+
+const prisma = new PrismaClient()
+
+export const getCourses = expressAsyncHandler(async (req: Request, res: Response) => {
+    const courses = await prisma.course.findMany()
+
+    res.json(courses)
+})
+
+export const createCourse = expressAsyncHandler(async (req: Request, res: Response) => {
+    const { data } = req.body
+    const course = await prisma.course.create({
+        data: {
+            title: data.title,
+            thumbnail: data.thumbnail ?? null,
+            description: data.description ?? null,
+            price: data.price ?? 0,
+            is_published: data.is_published,
+            user: { connect: { id: req.user.id } }
+        }
+    })
+
+    res.json(course)
+})
+
+export const updateCourse = expressAsyncHandler(async (req: Request, res: Response) => {
+    // pass
+})
+
+export const deleteCourse = expressAsyncHandler(async (req: Request, res: Response) => {
+    // pass
+})
